74ea1a305807a0b1789bbe6024ffaf92fdc9174e
Aurelien Tamas-Leloup <tamas-le@etud.insa-toulouse.fr>
2015-11-27 14:04:49 +0100

    Le split fonctionne

--- src/test/ModuleIndexation.java
+++ src/test/ModuleIndexation.java
@@ -5,17 +5,31 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ModuleIndexation {
 	public static void main(String[] args) {
 		File input = new File("./documents/CORPUS/D1.html");
-//		File file = new File(".");
-//		System.out.println(file.getAbsolutePath());
+
 		try {
 			Document doc =Jsoup.parse(input, "UTF-8");
-			Elements tags=doc.getElementsByTag("*");
-			System.out.println(doc.toString());
+			Elements elements = doc.getAllElements();
+			
+			StringBuilder sb = new StringBuilder();
+			for (Element e:elements){
+				if (!e.tagName().equals("script") || e.tagName().equals("style")){
+					sb.append(" "+e.text());
+				} 
+				
+			}
+			
+			String grosText = sb.toString();
+			System.out.println(grosText);
+			String [] texts=grosText.split("\\s+");
+			System.out.println(texts[34]);
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
