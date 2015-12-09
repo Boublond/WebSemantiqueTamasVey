95e2de11c1afbf914970377c7fde231dabf3bd7d
Aurelien Tamas-Leloup <tamas-le@etud.insa-toulouse.fr>
2015-12-09 12:06:20 +0100

    ModuleRecherche fonctionne !!

--- src/test/ModuleRecherche.java
+++ src/test/ModuleRecherche.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 
 public class ModuleRecherche {
 	private static final String path ="./documents/requetes/";
-	
+
 	private static String [] lireRequete(String requetename){
 		BufferedReader br;
 
@@ -22,27 +22,26 @@ public class ModuleRecherche {
 
 			while (line != null) {
 				sb.append(line);
-				sb.append(System.lineSeparator());
 				line = br.readLine();
 			}
 			String everything = sb.toString();
 			System.out.println(everything);
 			br.close();
-			return everything.split(",");
+			
+			return everything.split(", ");
 		}catch(Exception e){
 			e.printStackTrace();
 		}	
 		return null;
 
-		
+
 	}
 
-	public void CountWord (String mot) {
+	public static void CountWord (String mot) {
 		String url = "jdbc:mysql://localhost:3306/la_base";
 		String utilisateur = "user";
 		String motDePasse = "password";
 		Connection connexion = null;
-		File input = new File("./documents/CORPUS/D1.html");
 		try {
 			connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
 		} catch (SQLException e) {
@@ -51,26 +50,35 @@ public class ModuleRecherche {
 		}
 
 		try {
+
 			Statement statement = connexion.createStatement();
-			ResultSet resultat = statement.executeQuery( "SELECT count(mot) as nombre, num FROM dico WHERE mot='"+mot+"' GROUP BY num);");
+			String  query = "SELECT count(mot) as nombre, num FROM dico WHERE mot='"+mot+"' GROUP BY num;";
+			ResultSet resultat = statement.executeQuery(query );
+			resultat.beforeFirst();
 			while ( resultat.next() ) {
-			    int nombreDeMot = resultat.getInt( "nombre" );
-			    int idDoc = resultat.getInt( "num" );
-			    System.out.println("Il y a " + nombreDeMot + " le mot "+mot+" dans le document "+idDoc);
-			}	
+				
+				int nombreDeMot = resultat.getInt( "nombre" );
+				int idDoc = resultat.getInt( "num" );
+				System.out.println("Il y a " + nombreDeMot + " fois  le mot "+mot+" dans le document "+idDoc);
+
+			}
+			connexion.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+
 	}
-	
-	
+
+
 	public static void main(String[] args) {
 		String [] requeteWords=lireRequete("Q1");
 		for (int i=0;i<requeteWords.length;i++){
 			String mot = requeteWords[i];
+			System.out.println(mot);
+			CountWord(mot);
+
 		}
 
 	}
