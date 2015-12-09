d592efdfca32d450000688bfad5c77c60e5feda0
Boublond <Alex@ndr3>
2015-12-09 11:30:40 +0100

    Ajout de la fonction pour rechercher un mot

--- src/test/ModuleRecherche.java
+++ src/test/ModuleRecherche.java
@@ -1,7 +1,13 @@
 package test;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ModuleRecherche {
 	private static final String path ="./documents/requetes/";
@@ -31,6 +37,36 @@ public class ModuleRecherche {
 		
 	}
 
+	public void CountWord (String mot) {
+		String url = "jdbc:mysql://localhost:3306/la_base";
+		String utilisateur = "user";
+		String motDePasse = "password";
+		Connection connexion = null;
+		File input = new File("./documents/CORPUS/D1.html");
+		try {
+			connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Statement statement = connexion.createStatement();
+			ResultSet resultat = statement.executeQuery( "SELECT count(mot) as nombre, num FROM dico WHERE mot='"+mot+"' GROUP BY num);");
+			while ( resultat.next() ) {
+			    int nombreDeMot = resultat.getInt( "nombre" );
+			    int idDoc = resultat.getInt( "num" );
+			    System.out.println("Il y a " + nombreDeMot + " le mot "+mot+" dans le document "+idDoc);
+			}	
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+	}
+	
+	
 	public static void main(String[] args) {
 		lireRequete("Q1");
 
