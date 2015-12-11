45a97505ce309591d3a22b9e866472be02192014
Aurelien Tamas-Leloup <tamas-le@etud.insa-toulouse.fr>
2015-12-11 11:39:11 +0100

    Ajout de la classe document

---
+++ src/test/Document.java
@@ -0,0 +1,77 @@
+package test;
+
+public class Document implements Comparable<Document> {
+	private float pertinence;
+	private String name;
+	
+	
+	
+	
+	
+	@Override
+	public int compareTo(Document doc2) {
+		if (this.pertinence<doc2.pertinence){
+			return -1;
+		} else if (this.pertinence==doc2.pertinence) {
+			return 0;
+		} else {
+			return 1;
+		}
+		
+	}
+
+
+
+
+
+	public Document(float pertinence, String name) {
+		super();
+		this.pertinence = pertinence;
+		this.name = name;
+	}
+
+
+
+
+
+	public float getPertinence() {
+		return pertinence;
+	}
+
+
+
+
+
+	public void setPertinence(float pertinence) {
+		this.pertinence = pertinence;
+	}
+
+
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Document [pertinence=" + pertinence + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
