b3bb22f16993291c197908ee5b2381fafd87bc36
Boublond <Alex@ndr3>
2016-01-07 15:21:29 +0100

    Ajout de la troncature à 7 !

--- src/modules/ModuleIndexation.java
+++ src/modules/ModuleIndexation.java
@@ -42,7 +42,7 @@ public class ModuleIndexation {
 	}
 	
 	//TODO ins�rer les nom des balises dans la base/ retirer les determinants et mots inutiles 
-	public static void indexSimple(){
+	public static void indexSimple(boolean withTronc){
 
 
 		int id =0;
@@ -51,6 +51,10 @@ public class ModuleIndexation {
 		File folder = new File("./documents/CORPUS/");
 		
 		String []filesName=folder.list();
+		Statement statement;
+		try {
+			statement = connexion.createStatement();
+
 
 		for (int i=0;i<filesName.length;i++){
 
@@ -58,7 +62,7 @@ public class ModuleIndexation {
 
 			File input = new File("./documents/CORPUS/"+filesName[i]);
 			System.out.println(input.getAbsolutePath());
-			try {
+
 
 				Document doc =Jsoup.parse(input, "UTF-8");
 
@@ -73,12 +77,11 @@ public class ModuleIndexation {
 						text = text.replaceAll("[\n\t\b\r]", " ");
 						text = text.replaceAll("»|«|[0-9]| alors | au | aucuns | aussi | autre | avant | avec | avoir | bon | car | ce | cela | ces | ceux | chaque | ci | comme | comment | dans | de | des | du | dedans | dehors | depuis | devrait | doit | donc | dos | début | elle | elles | en | encore | essai | est | et | eu | fait | faites | fois | font | hors | ici | il | ils | je | juste | la | le|les | leur | là | ma | maintenant | mais | mes | mine | moins | mon | mot | même | ne | ni | nommés | notre | nos | nous | ok | ou | où | par | parce | pas | peut | peu | plupart | pour | pourquoi | quand | que | quel | quelle | quelles | quels | qui | sa | sans | ses | seulement | si | sien | son | sont | sous | soyez | sujet | sur | ta | tandis | tellement | tels | tes | ton | tous | tout | trop | très | tu | voient | vont | votre | vous | vu | ça | étaient | état | étions | été | être | a | à | l'| d'| j'| m'| n'|[ .?!$€,;:\'\"&-]+", " ");
 						StringTokenizer str = new StringTokenizer(text);
-						Statement statement = connexion.createStatement();
 						while (str.hasMoreTokens()){
 							String nextToken =str.nextToken();
 							if(!nextToken.equals(" ")){
 								//Troncature à 7 ! 
-								if (nextToken.length() > 7){
+								if (withTronc && nextToken.length() > 7){
 									nextToken = nextToken.substring(0, nextToken.length() - (nextToken.length()-7));
 								}
 								int statut = statement.executeUpdate( "INSERT INTO dico (id, mot, doc, balise) VALUES ("+id+",'"+nextToken+"','"+filesName[i]+"','"+e.nodeName()+"');" );
@@ -113,18 +116,18 @@ public class ModuleIndexation {
 				//				 
 				//			}
 
-			} catch ( SQLException e ) {
-				e.printStackTrace();
-				//    Gérer les éventuelles erreurs ici 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
 
-			}
 		}
 		closeConnexionToDB();
+		} catch ( SQLException e ) {
+			e.printStackTrace();
+			//    Gérer les éventuelles erreurs ici 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
 
+		}
 	}
 
 	
@@ -139,6 +142,6 @@ public class ModuleIndexation {
 
 	public static void main(String[] args) {
 
-		indexSimple();
+		indexSimple(true);
 	}
 }
