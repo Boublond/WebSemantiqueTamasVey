9a36bd4c880a5eb6638abe0c1baf5c5a6db17d53
Aurelien Tamas-Leloup <tamas-le@etud.insa-toulouse.fr>
2016-01-13 12:36:44 +0100

    Ajout d'une fonction

--- src/align/TestAlign.java
+++ src/align/TestAlign.java
@@ -1,13 +1,23 @@
 package align;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
+import org.semanticweb.owl.align.Alignment;
 import org.semanticweb.owl.align.AlignmentException;
 import org.semanticweb.owl.align.AlignmentProcess;
+import org.semanticweb.owl.align.AlignmentVisitor;
 
 import fr.inrialpes.exmo.align.impl.method.NameEqAlignment;
+import fr.inrialpes.exmo.align.impl.renderer.RDFRendererVisitor;
 
 public class TestAlign {
 	public static void main(String[] args) {
@@ -28,6 +38,19 @@ public class TestAlign {
 		alignment . init ( onto1 , onto2 ) ;
 		alignment . align (null , new Properties () ) ;
 		System . out . println ("Num corresp . générées : " + alignment . nbCells () ) ;
+	}
+
+	public static void render ( Alignment alignment ) throws
+	FileNotFoundException , UnsupportedEncodingException , AlignmentException
+	{
+		PrintWriter writer ;
+		FileOutputStream f = new FileOutputStream (new File ("C:/Users/Aur�lien/Downloads/align/test.rdf") ) ;
+		writer = new PrintWriter ( new BufferedWriter ( new OutputStreamWriter (f ,
+				"UTF -8" )) , true ) ;
+		AlignmentVisitor renderer = new RDFRendererVisitor ( writer ) ;
+		alignment . render ( renderer ) ;
+		writer . flush () ;
+		writer . close () ;
 	}
 
 }
